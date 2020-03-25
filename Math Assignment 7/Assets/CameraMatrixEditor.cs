5bba41936d0b6cd7b94bf9df90c82c8a1b65515f
Gabriel Groenendaal <gfg243@nyu.edu>
2020-03-25 17:43:14 -0400

    Okie dokie

---
+++ Math Assignment 7/Assets/CameraMatrixEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMatrixEditor : MonoBehaviour
+{
+    private const double V = 1.0;
+
+    // Start is called before the first frame update
+
+    Matrix4x4 MatRotate = Matrix4x4.identity;
+    Matrix4x4 MatScale = Matrix4x4.identity;
+    Matrix4x4 MatTranslation = Matrix4x4.identity;
+    Matrix4x4 MatYshear = Matrix4x4.identity;
+    Matrix4x4 MatXShear = Matrix4x4.identity;
+
+    Matrix4x4 OriginalProjection;
+
+    Camera camera;
+
+    public float rotation_angle;
+    public Vector2 scale = Vector2.zero;
+    public Vector2 shearing = Vector2.zero;
+    public Vector2 translation = Vector2.zero;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        OriginalProjection = camera.projectionMatrix;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MatScale[0,0] = scale.x;
+        MatScale[1,1] = scale.y;
+
+        MatXShear[0,1] = Mathf.Tan(shearing.x);
+        MatYshear[1,0] = Mathf.Tan(shearing.y);
+
+        MatTranslation[0,2] = translation.x;
+        MatTranslation[1,2] = translation.y;
+
+        MatRotate[0,0] = Mathf.Cos(rotation_angle);
+        MatRotate[0,1] = Mathf.Sin(rotation_angle);
+        MatRotate[1,0] = -1 * Mathf.Sin(rotation_angle);
+        MatRotate[1,1] = MatRotate[0,0];
+
+        camera.projectionMatrix = OriginalProjection * MatScale * MatXShear * MatYshear * MatTranslation * MatRotate;
+
+    }
+}
---
+++ Math Assignment 7/Assets/TransformExampleScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformExampleScript : MonoBehaviour
+{
+    public Vector3 translation;
+    public Vector3 rotation;
+    public Vector3 scale = Vector3.one;
+
+    MeshFilter mymesh;
+    Vector3[] originalVertices;
+    Vector3[] newVertices;
+
+    Matrix4x4 m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mymesh = GetComponent<MeshFilter>();
+        originalVertices = mymesh.mesh.vertices;
+        newVertices = new Vector3[originalVertices.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m = Matrix4x4.identity;
+        Quaternion qrotation = Quaternion.Euler(rotation);
+        m.SetTRS(translation, qrotation, scale);
+
+        for (int i = 0; i < originalVertices.Length; i++){
+            newVertices[i] = m.MultiplyPoint3x4(originalVertices[i]);
+        }
+        mymesh.mesh.vertices = newVertices;
+    }
+}
